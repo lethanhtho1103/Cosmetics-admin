3e1be6eafed5942c6c3597f6e497f2d2f48b2e9b
Thanh Tho <letanhtho1953@gmail.com>
2024-10-04 11:35:04 +0700

    Chỉnh sửa tổng số người dùng và người dùng mới trang user

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,6 +1,5 @@
-import { UserCheck, UserPlus, UsersIcon, UserX } from "lucide-react";
+import { UserPlus, UsersIcon } from "lucide-react";
 import { motion } from "framer-motion";
-
 import Header from "../components/common/Header";
 import StatCard from "../components/common/StatCard";
 import UsersTable from "../components/users/UsersTable";
@@ -8,30 +7,61 @@ import UserDemographicsChart from "../components/users/UserDemographicsChart";
 import { useEffect, useState } from "react";
 import authService from "../services/authService";
 
-const userStats = {
-  totalUsers: 152845,
-  newUsersToday: 243,
-  activeUsers: 98520,
-  churnRate: "2.4%",
-};
-
 const UsersPage = () => {
   const [users, setUsers] = useState([]);
+  const [userStats, setUserStats] = useState({
+    totalUsers: 0,
+    newUsersToday: 0,
+  });
 
   const handleGetAllUsers = async () => {
     const res = await authService.getAllUsers();
     setUsers(res?.data);
   };
 
+  const calculateUserStats = (users) => {
+    const today = new Date();
+    const startOfToday = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate()
+    ).getTime();
+
+    const startOfTomorrow = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() + 1
+    ).getTime();
+
+    const totalUsers = users.length;
+    const newUsersToday = users.filter((user) => {
+      const createdAt = new Date(user?.createdAt).getTime();
+      console.log(user?.createdAt);
+      return createdAt >= startOfToday && createdAt < startOfTomorrow;
+    }).length;
+
+    setUserStats((prevStats) => ({
+      ...prevStats,
+      totalUsers,
+      newUsersToday,
+    }));
+  };
+
   useEffect(() => {
     handleGetAllUsers();
   }, []);
+
+  useEffect(() => {
+    if (users.length > 0) {
+      calculateUserStats(users);
+    }
+  }, [users]);
+
   return (
     <div className="flex-1 overflow-auto relative z-10">
       <Header title="Người Dùng" />
 
       <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
-        {/* THỐNG KÊ NGƯỜI DÙNG */}
         <motion.div
           className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
           initial={{ opacity: 0, y: 20 }}
@@ -50,29 +80,16 @@ const UsersPage = () => {
             value={userStats.newUsersToday}
             color="#10B981"
           />
-          <StatCard
-            name="Người Dùng Hoạt Động"
-            icon={UserCheck}
-            value={userStats.activeUsers.toLocaleString()}
-            color="#F59E0B"
-          />
-          <StatCard
-            name="Tỉ Lệ Rời Bỏ"
-            icon={UserX}
-            value={userStats.churnRate}
-            color="#EF4444"
-          />
         </motion.div>
 
         <UsersTable users={users} />
 
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-8">
-          {/* <UserGrowthChart />
-          <UserActivityHeatmap /> */}
           <UserDemographicsChart />
         </div>
       </main>
     </div>
   );
 };
+
 export default UsersPage;
